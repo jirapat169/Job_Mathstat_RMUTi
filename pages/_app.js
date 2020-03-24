56fb6d8fa8c93933c20ddcf3e17a90a14b3ecd60
jirapat169 <james.jirapat@outlook.com>
2020-03-25 02:57:48 +0700

    add firebase

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,20 @@ import { Provider } from "react-redux";
 import React from "react";
 import { store } from "../app/redux";
 import withRedux from "next-redux-wrapper";
+import firebase from "firebase/app";
+import "firebase/database";
+
 import "../app/style.scss";
 
 const basePath = require("./../base_path");
+const settings = { timestampsInSnapshots: true };
 
 class MyApp extends App {
   constructor(props) {
     super(props);
+    this.state = {
+      news: {}
+    };
   }
 
   componentDidMount() {}
@@ -25,10 +32,32 @@ class MyApp extends App {
 
   render() {
     const { Component, pageProps } = this.props;
+    const config = {
+      apiKey: "AIzaSyCONB0piLBtDWrd0X6EQM3X4j5GfhUD5Vc",
+      authDomain: "myapp-c233c.firebaseapp.com",
+      databaseURL: "https://myapp-c233c.firebaseio.com",
+      projectId: "myapp-c233c",
+      storageBucket: "myapp-c233c.appspot.com",
+      messagingSenderId: "718102188961",
+      appId: "1:718102188961:web:9f9d7084e872ad3da6b1e0"
+    };
+    if (!firebase.apps.length) {
+      firebase.initializeApp(config);
+
+      const news = firebase.database().ref("/news");
+      news.on("value", value => {
+        this.setState({ news: value });
+      });
+    }
 
     return (
       <Provider store={store}>
-        <Component {...pageProps} {...this.props} />
+        <Component
+          {...pageProps}
+          {...this.props}
+          firebase={firebase}
+          news={this.state.news}
+        />
         <script src={`${basePath()}assets/js/all.js`} defer></script>
         <script
           src={`${basePath()}assets/js/jquery-3.4.1.slim.min.js`}
